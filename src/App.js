b982c96cac0062e1543cc20f1fb4fd11bc936283
Germán Escobar <german.escobarc@gmail.com>
2021-11-30 15:53:23 -0500

    Initial import

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import PrivateRoute from './PrivateRoute';
 import './App.css';
+
+const Table = ({ children }) => {
+  return (
+    <table>
+      {children}
+    </table>
+  )
+}
+
+const Row = ({ children }) => {
+  return (
+    <tr>
+      <td>{children}</td>
+    </tr>
+  )
+}
+
+const Products = () => {
+  return (
+    <>
+      <h1>Products</h1>
+      <Table>
+        <Row>
+          <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+          </ul>
+        </Row>
+        <Row>dsdsd</Row>
+      </Table>
+    </>
+  )
+}
+const Users = ({title}) => <h1>{title}</h1>
+const Login = ({location}) => {
+  return <h1>Login from {location.state.from.pathname}</h1>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" render={() => <h1>Root</h1>} />
+        <Route exact path="/login" component={Login} />
+        <PrivateRoute path="/products" component={Products} />
+        <PrivateRoute exact path="/users" component={Users} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/PrivateRoute.js
@@ -0,0 +1,21 @@
+import { Route, Redirect } from 'react-router-dom'
+
+export default function PrivateRoute({ component: Component, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        localStorage.getItem("auth") ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: '/login',
+              state: { from: props.location },
+            }}
+          />
+        )
+      }
+    />
+  );
+};
